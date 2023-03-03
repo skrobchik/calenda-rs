6f1b580cbb08b443744ce028b93462b35cb02e96
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2023-03-03 12:30:12 -0800

    impl default and serialize for school schedule

--- src/app.rs
+++ src/app.rs
@@ -334,6 +334,7 @@ impl Default for MyApp {
       calendar_view_type: Default::default(),
       calendar_view_semester: Default::default(),
       calendar_view_class: Default::default(),
+      school_schedule: SchoolSchedule::,
     }
   }
 }
