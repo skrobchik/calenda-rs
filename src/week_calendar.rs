95943aad423723d2978d74ba3f5a6ce1a88c4e22
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2023-03-03 12:44:12 -0800

    fix serialize deserialize for DaySchedule

--- src/week_calendar.rs
+++ src/week_calendar.rs
@@ -1,6 +1,6 @@
 use std::ops::Index;
 
-use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_big_array::BigArray;
 
 use crate::timeslots::*;
@@ -8,83 +8,82 @@ use crate::timeslots::*;
 pub const DAY_COUNT: usize = 7; // 7 days in a week
 
 #[derive(Serialize, Deserialize, Clone, Copy)]
-struct DaySchedule<T: Serialize + DeserializeOwned> {
-    #[serde(with = "BigArray")]
-    data: [T; TIMESLOT_COUNT]
+struct DaySchedule<T> {
+  #[serde(
+    bound(serialize = "T: Serialize", deserialize = "T: Deserialize<'de>"),
+    with = "BigArray"
+  )]
+  data: [T; TIMESLOT_COUNT],
 }
 
-impl<T: Serialize + DeserializeOwned + Default + Copy> Default for DaySchedule<T> {
-    fn default() -> Self {
-        let init: T = Default::default();
-        Self { data: [init; TIMESLOT_COUNT] }
+impl<T: Default + Copy> Default for DaySchedule<T> {
+  fn default() -> Self {
+    let init: T = Default::default();
+    Self {
+      data: [init; TIMESLOT_COUNT],
     }
+  }
 }
 
 #[derive(Serialize, Clone, Copy)]
 pub struct WeekCalendar<T: Serialize + DeserializeOwned> {
-    data: [DaySchedule<T>; DAY_COUNT],
+  data: [DaySchedule<T>; DAY_COUNT],
 }
 
 impl<T: Serialize + DeserializeOwned + Default + Copy> Default for WeekCalendar<T> {
-    fn default() -> Self {
-        Self { data: Default::default() }
+  fn default() -> Self {
+    Self {
+      data: Default::default(),
     }
-}
-
-impl<'de, T: Serialize + DeserializeOwned> Deserialize<'de> for WeekCalendar<T> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de> {
-        todo!()
-    }
+  }
 }
 
 #[derive(Clone, Copy)]
 pub enum Weekday {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
 }
 
 pub fn weekday_index(day: &Weekday) -> usize {
-    match day {
-        Weekday::Monday => 0,
-        Weekday::Tuesday => 1,
-        Weekday::Wednesday => 2,
-        Weekday::Thursday => 3,
-        Weekday::Friday => 4,
-        Weekday::Saturday => 5,
-        Weekday::Sunday => 6,
-    }
+  match day {
+    Weekday::Monday => 0,
+    Weekday::Tuesday => 1,
+    Weekday::Wednesday => 2,
+    Weekday::Thursday => 3,
+    Weekday::Friday => 4,
+    Weekday::Saturday => 5,
+    Weekday::Sunday => 6,
+  }
 }
 
 impl TryFrom<usize> for Weekday {
-    type Error = ();
+  type Error = ();
 
-    fn try_from(value: usize) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(Weekday::Monday),
-            1 => Ok(Weekday::Tuesday),
-            2 => Ok(Weekday::Wednesday),
-            3 => Ok(Weekday::Thursday),
-            4 => Ok(Weekday::Friday),
-            5 => Ok(Weekday::Saturday),
-            6 => Ok(Weekday::Sunday),
-            _ => Err(()),
-        }
+  fn try_from(value: usize) -> Result<Self, Self::Error> {
+    match value {
+      0 => Ok(Weekday::Monday),
+      1 => Ok(Weekday::Tuesday),
+      2 => Ok(Weekday::Wednesday),
+      3 => Ok(Weekday::Thursday),
+      4 => Ok(Weekday::Friday),
+      5 => Ok(Weekday::Saturday),
+      6 => Ok(Weekday::Sunday),
+      _ => Err(()),
     }
+  }
 }
 
 pub trait GetDay<T> {
-    fn get_day(&self, day: &Weekday) -> &[T; TIMESLOT_COUNT];
+  fn get_day(&self, day: &Weekday) -> &[T; TIMESLOT_COUNT];
 }
 
 impl<T: Serialize + DeserializeOwned> GetDay<T> for WeekCalendar<T> {
-    fn get_day(&self, day: &Weekday) -> &[T; TIMESLOT_COUNT] {
-        &self.data.get(weekday_index(day)).unwrap().data
-    }
+  fn get_day(&self, day: &Weekday) -> &[T; TIMESLOT_COUNT] {
+    &self.data.get(weekday_index(day)).unwrap().data
+  }
 }
